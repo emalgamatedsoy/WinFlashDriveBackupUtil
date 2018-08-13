1978b668bea836ad97094f99e65b45e0e28a8c40
unknown <mcsherry.patrick@gmail.com>
2018-08-13 13:29:28 -0400

    Adding the source for the program

---
+++ BackupUtility/BackupUtility/BackupFilesOutput.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackupUtility
+{
+    public partial class BackupFilesOutput : Form
+    {
+        public HashSet<String> files;
+        public BackupFilesOutput(HashSet<String> files)
+        {
+            this.files = files;
+            InitializeComponent();
+            OutputFilesVerbose();
+        }
+
+        public void OutputFilesVerbose()
+        {
+            verboseFileOutputTB.Text = "Copied " + files.Count + " files: \n\n";
+            foreach (string file in files)
+            {
+                verboseFileOutputTB.Text += file + "\n";
+            }
+        }
+        
+    }
+}
